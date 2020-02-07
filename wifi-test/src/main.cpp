fae39b27fce242b55d1d23cb063dbe5a6bce3f34
Arkadiusz Suchenia <arek@arnonedev.pl>
2020-02-07 16:32:11 +0100

    WiFi connection - not working

--- wifi-test/src/main.cpp
+++ wifi-test/src/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
+#include <WiFiEsp.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  WiFi.init(&Serial1);
+  WiFi.begin("Arek_Room", NULL);
+
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("Connected to the WiFi network");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("Writing network info");
+  Serial.println(WiFi.SSID());
+  Serial.println(WiFi.localIP());
+  delay(2000);
 }
