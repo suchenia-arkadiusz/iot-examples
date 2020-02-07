9dc1fa442e4a3ca56fd8bd491c47363ab59027b1
Arkadiusz Suchenia <arek@arnonedev.pl>
2020-02-07 14:34:48 +0100

    dded first program - print line in serial monitor

--- first-project/src/main.cpp
+++ first-project/src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("App started");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("In loop");
+  delay(2000);
 }
