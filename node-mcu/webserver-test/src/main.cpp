599dc55c5bd7331437c5b5e6361cb77bd0ad3566
Arkadiusz Suchenia <arek@arnonedev.pl>
2020-02-10 12:13:30 +0100

    Added endpoints to controll led using server endpoints

--- node-mcu/webserver-test/src/main.cpp
+++ node-mcu/webserver-test/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
 ESP8266WebServer server(80);
@@ -7,14 +8,44 @@ void handleLed() {
 
 }
 
+void configureWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.hostname("01-ESP-SmartLight");
+  WiFi.begin("Arek_Room");
+
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println(WiFi.localIP());
+}
+
+void configureEndpoints() {
+  pinMode(2, OUTPUT);
+
+  server.on("/led/on", []() {
+    digitalWrite(2, LOW);
+    server.send(200, "text/plain", "Led is on");
+  });
+
+  server.on("/led/off", []() {
+    digitalWrite(2, HIGH);
+    server.send(200, "text/plain", "Led is off");
+  });
+}
+
 void setup() {
   Serial.begin(115200);
+
+  configureWiFi();
   
   server.on("/", []() {
     server.send(200, "text/plain", "It works");
   });
 
-  server.on("/led", handleLed);
+  configureEndpoints();
+
   server.begin();
 }
 
