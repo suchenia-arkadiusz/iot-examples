f20e63e867f9a0230df9c5eff9aaaffb540ceea6
Arkadiusz Suchenia <arek@arnonedev.pl>
2020-02-12 13:56:46 +0100

    Added postgresql connection settings
    Added LED blinking if temperature is proper or to high

--- node-mcu/temperature-humidity-pressure-test/src/main.cpp
+++ node-mcu/temperature-humidity-pressure-test/src/main.cpp
@@ -7,16 +7,20 @@
 #define BME_MISO D4
 #define BME_MOSI D2
 #define BME_CS D3
+#define RED_LED D5
+#define BLUE_LED D6
 
 #define SAELEVELPRESSURE_HPA (1013.25)
 
 Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);
 
 unsigned long delayTime;
+float temp;
 
 void printValues() {
   Serial.print("Temperature: ");
-  Serial.print(bme.readTemperature());
+  temp = bme.readTemperature();
+  Serial.print(temp);
   Serial.println(" C");
   
   Serial.print("Pressure: ");
@@ -32,10 +36,20 @@ void printValues() {
   Serial.println(" %");
   
   Serial.println();
+
+  if(temp > 23.0F) {
+    digitalWrite(RED_LED, HIGH);
+    digitalWrite(BLUE_LED, LOW);
+  } else {
+    digitalWrite(BLUE_LED, HIGH);
+    digitalWrite(RED_LED, LOW);
+  }
 }
 
 void setup() {
   Serial.begin(115200);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
   Serial.println("BME280 Test");
 
   bool status;
